8cc882f61d020e8e8d3243be3a1087bdc83c99f5
Chinmayee2442 <97032666+Chinmayee2442@users.noreply.github.com>
2023-05-28 08:16:59 +0530

    Add files via upload

---
+++ matrixmult.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define row 3
+#define col 3
+void read(int mat[row][col]);
+void print(int mat[row][col]);
+void mult(int mat1[row][col],int mat2[row][col]);
+
+void main()
+{
+    int mat1[row][col],mat2[row][col];
+    printf("enter the elements for 1st matrix\n");
+    read(mat1);
+    print(mat1);
+     printf("enter the elements for 2nd matrix\n");
+    read(mat2);
+    print(mat2);
+    mult(mat1,mat2);
+}
+void read(int mat[row][col])
+{
+   int x,y;
+   for(x=0;x<row;x++)
+   {
+       for(y=0;y<col;y++)
+       {
+           scanf("%d",(*(mat+x)+y));
+       }
+   }
+}
+ void print(int mat[row][col])
+ {
+     int x,y;
+   for(x=0;x<row;x++)
+   {
+       for(y=0;y<col;y++)
+       {
+           printf("%d ",*(*(mat+x)+y));
+       }
+       printf("\n");
+   }
+}
+void mult(int mat1[row][col],int mat2[row][col])
+{
+   int x,y,k,sum=0,res[row][col];
+   for(x=0;x<row;x++)
+   {
+       for(y=0;y<col;y++)
+       {
+          for(k=0;k<col;k++)
+           {
+             //printf("numbers= %d\t %d\n",(*(*(mat1+x)+k)),(*(*(mat2+k)+y)));
+             sum=sum+((*(*(mat1+x)+k)) * (*(*(mat2+k)+y)));
+
+           }
+           //printf("%d\t",sum);
+           *(*(res+x)+y)=sum;
+           sum=0;
+       }
+   }
+   printf("\n");
+   print(res);
+}
+//1 2 3 4 5 6 7 8 9
