1a0698915fbcbaf0eb20cf16a0e45b3f26fb4672
Chinmayee2442 <97032666+Chinmayee2442@users.noreply.github.com>
2023-05-28 20:54:11 +0530

    Update find_replace.c

--- find_replace.c
+++ find_replace.c
@@ -28,7 +28,6 @@ int count(char str[])
         countt++;
         i++;
     }
-    //printf("%d",count);
     return countt;
 }
 void key(char str[][100])
@@ -36,8 +35,6 @@ void key(char str[][100])
     char keey[26],arr[10],abcd[26];
     printf("enter the key string\n");
     gets(keey);
-    //int f=count(keey);
-    //printf("f=%d\n",f);
     int i=0,j=0,k=0,l,n=0;
    for(int i=0;i<10;i++)
    { j=0;
@@ -54,8 +51,6 @@ void key(char str[][100])
                     while(keey[n]=='\0')
                     {
                         printf("LINE IS %s\n",str[i]);
-                       // arr[a]=str[i];
-                       // print("\n%s",arr[a]);
                        int a=i;
                         break;
                     }
@@ -67,26 +62,6 @@ void key(char str[][100])
     }
     //int f=count(str[i]); printf("%d\n",f);
    }
- /*int f=count(str[i+1]);
- int b=i+1;
- printf("%d\n",b);
- printf("%d\n",f);
- int s;
- j=0;
-while(str[b][j]!='\0')
-{
-    for(s=0;s<=f;s++)
-    {
-        arr[s]=str[b][j];
-        printf("%s",str[j]);
-        j++;
-    }
-}
-for(s=0;s<=f;s++)
-    {
-       printf("%s",arr[s]);
-    }*/
-
 printf("enter the string to be replaced\n");
 gets(abcd);
 for(int i=0;i<10;i++)
@@ -117,14 +92,4 @@ for(int i=0;i<10;i++)
     }
    }
 }
-/*
-cause i,i,i'm
- in the stars tonight
- so watch me bring the star
- and set the night alight
- shoes on get up in the morn
- cup of milk, lets rock n roll
- king kong kick the drum
- rolling on like a rolling stone..i dunno what to type
- sing-song when i'm walking home
- jump up to the top, lebron*/
+
