a96bbb7295a66787ce4c5847b91e4be4c78a3252
Chinmayee2442 <97032666+Chinmayee2442@users.noreply.github.com>
2023-05-28 21:05:56 +0530

    Update duplicare.c

--- duplicare.c
+++ duplicare.c
@@ -1,3 +1,4 @@
+//1st method
 #include<stdio.h>
 void duplicate(int a[26]);
 int main()
@@ -14,18 +15,6 @@ int main()
 void duplicate(int a[26])
 {
     int i=0,j,c=0,max=0,ab[9];
-    /*for(i=0;i<25;i++)
-    {
-        c=0;
-        for(j=i;j<25;j++)
-        {
-            if(a[i]==a[j])
-            {
-                c++;
-            }
-        }
-        printf("%d is repeated %d times\n",a[i],c);
-    }*/
     while(i<25)
     { c=0;
         for(j=i+1;j<25;j++)
@@ -36,19 +25,12 @@ void duplicate(int a[26])
             }
             else
             {
-                // printf("\n%d",j);
-               // i=j;
                 break;
             }
-            //break;
         }
        printf("\n%d is repeated %d times",a[i],c+1);
-       //printf("\n%d",j);
-      /* if((c+1)>max)
-       {
-           max=(c+1);
-       }*/
        i=j;
     }
 }
 /* 1 1 2 2 2 3 3 3 4 4 4 4 4 5 5 6 6 6 6 6 7 7 7 7 7 */
+
